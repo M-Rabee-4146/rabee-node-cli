f6d7ce12509b4dea8334a5a6f6044fce99231b5c
M-Rabee-4146 <techclub9t@gmail.com>
2026-03-28 22:18:23 +0500

    Minor: Bug fix, Package.json Update

--- template/frontendRedux/src/config/branding.js
+++ template/frontendRedux/src/config/branding.js
@@ -4,7 +4,7 @@ export const branding = {
     shortName: "RNCLI",
     developerName: "Muhammad Rabee",
     description: "A professional byproduct for creating fast, scalable, and secure React-Redux templates.",
-    version: "2.0.0",
+    version: "2.4.0",
     copyright: `© ${new Date().getFullYear()} Rabee Node CLI. All rights reserved.`,
     github: "https://github.com/M-Rabee-4146/rabee-node-cli",
     license: "ISC",
